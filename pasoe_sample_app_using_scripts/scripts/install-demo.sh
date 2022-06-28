1573a52f8deff56ba0a0e590648dff3fdee34911
edselg <egarcia@progress.com>
2022-06-28 08:39:41 -0400

    Add code to install Docker

--- pasoe_sample_app_using_scripts/scripts/install-demo.sh
+++ pasoe_sample_app_using_scripts/scripts/install-demo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPTS=`dirname $0`
+CONTAINER_ENGINE="${1:-podman}"
 
 if [ "$S3_BUCKET" != "" ]
 then
@@ -12,9 +13,27 @@ then
   $SCRIPTS/webfiles_copy_files.sh
 fi
 
+if [ "$CONTAINER_ENGINE" == "docker" ]
+then
+  $SCRIPTS/install-docker.sh
+fi
+
+if [ "$CONTAINER_ENGINE" == "podman" ]
+then
+  $SCRIPTS/install-podman.sh
+fi
+
 $SCRIPTS/install-podman.sh
 $SCRIPTS/load-containers.sh
 #
 $SCRIPTS/install-openedge.sh
 $SCRIPTS/install-pasoe-sample-app.sh
+
+if [ "$CONTAINER_ENGINE" == "docker" ]
+then
+  newgrp docker << EOT
 $SCRIPTS/run-pasoe-sample-app.sh
+EOT
+else
+  $SCRIPTS/run-pasoe-sample-app.sh
+fi
--- pasoe_sample_app_using_scripts/scripts/webfiles_copy_files.sh
+++ pasoe_sample_app_using_scripts/scripts/webfiles_copy_files.sh
@@ -6,6 +6,9 @@ mkdir -p ~/Downloads
 cd ~/Downloads
 for file in PROGRESS_OE_12.2.9_LNX_64.tar.gz PROGRESS_PASOE_CONTAINER_IMAGE_12.2.9_LNX_64.zip response.ini
 do
-  echo Downloading $file
-  curl -sO ${WEBFILES}/$file
+  if [ ! -f ~/Downloads/$file ]
+  then
+    echo Downloading $file
+    curl -sO ${WEBFILES}/$file
+  fi
 done
