63690a5b8283271cacbaa5b031577a238713f89e
edselg <egarcia@progress.com>
2022-12-01 15:48:20 +0000

    OpenTelemetry Demo (metrics)

--- scripts/create_oepas1.sh
+++ scripts/create_oepas1.sh
@@ -7,15 +7,21 @@ then
   echo "Creating PASOE instance (oepas1)..."
   export DLC=/psc/dlc
   export PATH=$DLC/bin:$PATH
-  cd /psc/wrk
+
   if [ -f /files/oepas1.tar.gz ]
   then
+    cd /
     tar xzf /files/oepas1.tar.gz
+    cp /vagrant/files/openedge.properties /psc/wrk/oepas1/conf
   else
+    cd /psc/wrk
     time pasman create -v oepas1
     cp /vagrant/files/openedge.properties /psc/wrk/oepas1/conf
     cp /vagrant/files/customer.p /psc/wrk/oepas1/openedge
-    tar czf /files/oepas1.tar.gz oepas1
+    cd /
+    tar czf /files/oepas1.tar.gz psc/dlc/servers/pasoe/conf/instances.unix psc/wrk/oepas1
   fi
+
+  cd /psc/wrk
   ./oepas1/bin/tcman.sh pasoestart -restart
 fi
