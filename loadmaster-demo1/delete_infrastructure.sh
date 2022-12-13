3866468ec4e451fb16f7dbd39e81a23fe982b49d
edselg <egarcia@progress.com>
2022-12-13 19:02:23 +0000

    Add create_image.sh to openedge-12.6.0

--- loadmaster-demo1/delete_infrastructure.sh
+++ loadmaster-demo1/delete_infrastructure.sh
@@ -8,13 +8,18 @@ function delete_vm ()
   az vm delete \
     --resource-group $RESOURCE_GROUP \
     --name $1 \
-    --yes
+    --yes > $TEMP/output.json
 }
 
 delete_vm WEB2
 delete_vm WEB1
 delete_vm PASOE2
 delete_vm PASOE1
-# delete_vm OEDB
+delete_vm OEDB
+delete_vm AccessVM
+
+az network public-ip delete --resource-group $RESOURCE_GROUP --name AccessVMPublicIP > $TEMP/output.json
+
+az network nsg delete --resource-group $RESOURCE_GROUP --name ${RESOURCE_GROUP}-nsg > $TEMP/output.json
 
 exit
