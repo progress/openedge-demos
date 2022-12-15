410308749dbe5c3bc636881b6a27d33a129bf258
edselg <egarcia@progress.com>
2022-12-15 13:07:18 +0000

    Update custom data to use latest version of deployment packages.

--- loadmaster-demo1/custom-data.pas
+++ loadmaster-demo1/custom-data.pas
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+NAME=pas.tar.gz
+REPO=openedge-demos
+API_URL=https://api.github.com/repos/progress/${REPO}/releases
+
+LATEST_VERSION=`curl -s ${API_URL}/latest | jq -r ".name"`
+URL=`curl -s ${API_URL}/tags/${LATEST_VERSION} | jq -r ".assets[] | select(.content_type == \"application/x-gzip\") | select(.name | contains(\"${NAME}\")) | .browser_download_url"`
+
 mkdir -p /install
 cd /install
-wget https://github.com/progress/openedge-demos/releases/download/v1.0.1/pas.tar.gz
-tar xzvf pas.tar.gz
+wget $URL
+tar xzvf $NAME
 
 OE_ENV=pasoe DBHostName=HOST /install/app/deploy.sh 
