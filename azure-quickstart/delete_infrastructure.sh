11ce9e22ca946a41c3faeb75da2b6b49665b5376
edselg <egarcia@progress.com>
2023-01-03 09:03:12 -0500

    Change Public IP Address to Static

--- azure-quickstart/delete_infrastructure.sh
+++ azure-quickstart/delete_infrastructure.sh
@@ -3,36 +3,31 @@
 RESOURCE_GROUP=1DemoResourceGroup
 TEMP=/tmp
 
-function delete_vm ()
-{
-  az vm delete \
-    --resource-group $RESOURCE_GROUP \
-    --name $1 \
-    --yes > $TEMP/output.json
-}
-
-# delete_vm WEB2
-# delete_vm WEB1
-# delete_vm PASOE2
-# delete_vm PASOE1
-# delete_vm OEDB
-# delete_vm AccessVM
-
 # az network public-ip delete --resource-group $RESOURCE_GROUP --name AccessVMPublicIP > $TEMP/output.json
 
 az vmss delete --resource-group $RESOURCE_GROUP --name WebServer
 az vmss delete --resource-group $RESOURCE_GROUP --name PASOE
 az vm delete --resource-group $RESOURCE_GROUP --name DB0 --yes
+az vm delete --resource-group $RESOURCE_GROUP --name AccessVM --yes
+az vm delete --resource-group $RESOURCE_GROUP --name LoadMaster --yes
 
-# az network nic delete --resource-group $RESOURCE_GROUP --name DB0-nic
-# az network public-ip delete --resource-group $RESOURCE_GROUP --name DB0-ip
+az network nic delete --resource-group $RESOURCE_GROUP --name DB0-nic
+az network nic delete --resource-group $RESOURCE_GROUP --name AccessVM-nic
+az network nic delete --resource-group $RESOURCE_GROUP --name LoadMaster-nic
 
-# az network nsg delete --resource-group $RESOURCE_GROUP --name webserver-nsg
-# az network nsg delete --resource-group $RESOURCE_GROUP --name pasoe-nsg
-# az network nsg delete --resource-group $RESOURCE_GROUP --name database-nsg
+az network nsg delete --resource-group $RESOURCE_GROUP --name webserver-nsg
+az network nsg delete --resource-group $RESOURCE_GROUP --name pasoe-nsg
+az network nsg delete --resource-group $RESOURCE_GROUP --name database-nsg
+az network nsg delete --resource-group $RESOURCE_GROUP --name accessvm-nsg
+az network nsg delete --resource-group $RESOURCE_GROUP --name LoadMaster-nsg
 
-az network vnet delete --resource-group $RESOURCE_GROUP --name ${RESOURCE_GROUP}-vnet
+az network public-ip delete --resource-group $RESOURCE_GROUP --name AccessVM-ip
+az network public-ip delete --resource-group $RESOURCE_GROUP --name LoadMaster-ip
+
+# az network vnet delete --resource-group $RESOURCE_GROUP --name ${RESOURCE_GROUP}-vnet
 exit
 
 # az network nsg delete --resource-group $RESOURCE_GROUP --name accessvm-nsg
 # az vm delete --resource-group $RESOURCE_GROUP --name AccessVM --yes
+
+
